e9ceb0d30d28d1193cd6e926a88734ad1a5db5ed
HappyTed <gra4ev.teodor@yandex.ru>
2026-02-04 09:31:12 +0300

    Kafka example

---
+++ kafka/consumer/readKafka.go
@@ -0,0 +1,56 @@
+package consumer
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Record struct {
+	Name   string `json:"name"`
+	Random int    `json:"random"`
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Need a Kafka topic name.")
+		return
+	}
+
+	partition := 0
+	topic := os.Args[1]
+	fmt.Println("Kafka topic:", topic)
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   []string{"localhost:9092"},
+		Topic:     topic,
+		Partition: partition,
+		MinBytes:  10e3,
+		MaxBytes:  10e6,
+	})
+	r.SetOffset(0)
+
+	context := context.Background()
+	for {
+		m, err := r.ReadMessage(context)
+		if err != nil {
+			fmt.Println("error:", err)
+			break
+		}
+		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+
+		temp := Record{}
+		err = json.Unmarshal(m.Value, &temp)
+
+		if err != kafka.NoReassignmentInProgress {
+			fmt.Println(err)
+			break
+		}
+		fmt.Printf("%T\n", temp)
+	}
+
+	r.Close()
+}
